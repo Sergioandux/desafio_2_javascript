44df6ac4dab385183a70eca2697a2a49136771f6
sergioandux <sergioandux@gmail.com>
2021-11-25 23:50:08 -0300

    finalizado el juego del tictactoe

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var CantidadDeJuegos = prompt("ingrese la cantidad de juegos que le gustaria realizar");
+var empate = 0;
+var victoria = 0;
+var derrota = 0;
 for (var i = 0; i<CantidadDeJuegos;i++) {
     var jugador1 = prompt("ingresa 'piedra', 'papel' o 'tijera'");
     var jugador2 = (parseInt(Math.random()*10))%3;
@@ -9,8 +12,8 @@ for (var i = 0; i<CantidadDeJuegos;i++) {
             jugador2 = "papel"
         } else {
             jugador2 = "tijera"
-        }}
-    var resultado
+        }};
+    var resultado;
     if (jugador1 == "piedra") {
         if (jugador2 == "piedra") {
             resultado = "empate"
@@ -20,8 +23,7 @@ for (var i = 0; i<CantidadDeJuegos;i++) {
             } else {
                 resultado = "has ganado!!"
             }
-        }
-        }
+        }};
     if (jugador1 == "tijera") {
         if (jugador2 == "tijera") {
             resultado = "empate"
@@ -30,9 +32,7 @@ for (var i = 0; i<CantidadDeJuegos;i++) {
                 resultado = "victoria jugador incognito"
             } else {
                 resultado = "has ganado!!"
-            }
-        }
-        }
+            }}};
     if (jugador1 == "papel") {
             if (jugador2 == "papel") {
                 resultado = "empate"
@@ -41,8 +41,27 @@ for (var i = 0; i<CantidadDeJuegos;i++) {
                     resultado = "victoria jugador incognito"
                 } else {
                     resultado = "has ganado!!"
-                }
-            }
-            };
+                }}};
     alert("jugaste: "+jugador1+", jugador incognito jugó: "+jugador2+", "+resultado);
-document.write(i)};
+    document.write((i+1)+" - "+resultado+"<br>")
+    if (resultado == "empate") {
+        empate = empate + 1
+    } else {
+        if (resultado == "has ganado!!") {
+            victoria = victoria + 1
+        } else {
+            derrota = derrota + 1
+        }
+    }
+};
+document.write("cantidad de empates: "+empate+"<br>"+"cantidad de victorias: "+victoria+"<br>"+"cantidad de derrotas: "+derrota);
+var resumenFinal
+if (victoria > derrota) {
+    resumenFinal = "HAS GANADO LA PARTIDAD, FELICITACIONES :)"
+} else { if (derrota > victoria) {
+    resumenFinal = "HAS PERDIDO LA PARTIDA :("
+} else {
+    resumenFinal = "ES UN EMPATE WEY, TODOS FELICES"
+}
+};
+document.write("<br>"+"``````````````"+resumenFinal+"´´´´´´´´´´´´´´´´´");
